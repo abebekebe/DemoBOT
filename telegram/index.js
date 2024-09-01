5159e324d452bb9cfa83ed5e398d38d8db2a96e5
abebekebe <119934941+abebekebe@users.noreply.github.com>
2024-09-01 12:45:55 +0300

    Update index.js

--- telegram/index.js
+++ telegram/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Bot is alive");
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
